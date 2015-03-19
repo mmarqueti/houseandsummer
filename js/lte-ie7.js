2fc68afc206f26909e1c0ec74ea94c0ede670152
marcelo-marqueti-movile <marcelo.marqueti@movile.com>
2015-03-18 21:58:37 -0300

    welcome

---
+++ js/lte-ie7.js
@@ -0,0 +1,3 @@
+window.onload=function(){function addIcon(el,entity){var html=el.innerHTML;el.innerHTML='<span style="font-family: \'icomoon\'">'+ entity+'</span>'+ html;}
+var icons={'icon-fullscreen-exit':'&#x21;','icon-grid-view':'&#x22;','icon-cog':'&#x23;','icon-html5':'&#x24;','icon-eye':'&#x25;','icon-heart':'&#x26;'},els=document.getElementsByTagName('*'),i,attr,html,c,el;for(i=0;i<els.length;i+=1){el=els[i];attr=el.getAttribute('data-icon');if(attr){addIcon(el,attr);}
+c=el.className;c=c.match(/icon-[^\s'"]+/);if(c){addIcon(el,icons[c[0]]);}}};
